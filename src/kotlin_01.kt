575a77f8eb373e6e59cdac43bb5d1cf02d7cdbdb
aadhil96 <aadhil.imam@gmail.com>
2018-02-23 09:06:10 -0800

    Initial commit

---
+++ src/kotlin_01.kt
@@ -0,0 +1,45 @@
+/**
+ * Created by Aadhil on 2/21/2018.
+ */
+
+package demo;
+
+fun main(args : Array<String>){
+    println("Hello World")
+
+    val name = "Khan" //mutable variables
+    val myAge = 20      //immutable variables
+
+    val bigInt:Int = Int.MAX_VALUE
+    val smallInt:Int = Int.MIN_VALUE
+    println("Biggest value :" + bigInt)
+    println("Smallest value : $smallInt")
+
+    val bigLong:Long = Long.MAX_VALUE
+    val smallLong:Long = Long.MIN_VALUE
+    println("Biggest Long value :" + bigLong)
+    println("Smallest Long value : $smallLong")
+
+    val bigDouble:Double = Double.MAX_VALUE
+    val smallDouble:Double = Double.MIN_VALUE
+    println("Biggest Double value :" + bigDouble)
+    println("Smallest Double value : $smallDouble")
+
+    val bigFloat:Float = Float.MAX_VALUE
+    val smallFloat:Float = Float.MIN_VALUE
+    println("Biggest Float value :" + bigFloat)
+    println("Smallest Float value : $smallFloat")
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
